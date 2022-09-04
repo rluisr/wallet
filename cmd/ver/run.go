90522f2cd04c6f957704d748f00c4dcff8478845
xh3b4sd <xh3b4sd@gmail.com>
2022-09-05 01:24:30 +0200

    fix variable name for version injection (#3)

--- cmd/ver/run.go
+++ cmd/ver/run.go
@@ -11,16 +11,16 @@ import (
 var (
 	rep = "https://github.com/phoebetron/wallet"
 	sha = "n/a"
-	ver = "n/a"
+	tag = "n/a"
 )
 
 type run struct{}
 
 func (r *run) run(cmd *cobra.Command, args []string) {
 	fmt.Fprintf(os.Stdout, "Git Sha       %s\n", sha)
+	fmt.Fprintf(os.Stdout, "Git Tag       %s\n", tag)
 	fmt.Fprintf(os.Stdout, "Repository    %s\n", rep)
-	fmt.Fprintf(os.Stdout, "Version       %s\n", ver)
-	fmt.Fprintf(os.Stdout, "Go Version    %s\n", runtime.Version())
 	fmt.Fprintf(os.Stdout, "Go Arch       %s\n", runtime.GOARCH)
 	fmt.Fprintf(os.Stdout, "Go OS         %s\n", runtime.GOOS)
+	fmt.Fprintf(os.Stdout, "Go Version    %s\n", runtime.Version())
 }
